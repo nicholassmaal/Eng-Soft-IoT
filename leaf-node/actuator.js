44ee7acea8611332b0d36e3951b923921934f010
Nicholas Smaal <nicholassmaal@gmail.com>
2017-07-22 21:56:02 -0300

    Expansão
    Esta extensão permite ao usuário utilizar todas as portas GPIO da placa Raspberry Pi 3b. Além disso, algumas correções foram feitas nos comentários. O arranjo das GPIOs foi escolhido respeitando a localização delas, sendo os Inputs de um lado e os Outputs de outro. Este arranjo é : Entradas (12;16;18;20;21;23;24;25), Saídas (04;05;06;13;17;19;22;26;27).

--- leaf-node/actuator.js
+++ leaf-node/actuator.js
@@ -34,18 +34,53 @@ var commandTypeArray = [{
 								}];
 
 // Portas de Saída
+
+var gpioPort04 = new GPIO.GPIOPort(04);
+gpioPort04.open(GPIO.PORT_DIRECTION.OUTPUT, function(){console.log("Porta 04 pronta.")});
+
+var gpioPort05 = new GPIO.GPIOPort(05);
+gpioPort05.open(GPIO.PORT_DIRECTION.OUTPUT, function(){console.log("Porta 05 pronta.")});
+
+var gpioPort06 = new GPIO.GPIOPort(06);
+gpioPort06.open(GPIO.PORT_DIRECTION.OUTPUT, function(){console.log("Porta 06 pronta.")});
+
+var gpioPort13 = new GPIO.GPIOPort(13);
+gpioPort13.open(GPIO.PORT_DIRECTION.OUTPUT, function(){console.log("Porta 13 pronta.")});
+
 var gpioPort17 = new GPIO.GPIOPort(17);
 gpioPort17.open(GPIO.PORT_DIRECTION.OUTPUT, function(){console.log("Porta 17 pronta.")});
+
+var gpioPort19 = new GPIO.GPIOPort(19);
+gpioPort19.open(GPIO.PORT_DIRECTION.OUTPUT, function(){console.log("Porta 19 pronta.")});
 
 var gpioPort22 = new GPIO.GPIOPort(22);
 gpioPort22.open(GPIO.PORT_DIRECTION.OUTPUT, function(){console.log("Porta 22 pronta.")});
 
+var gpioPort26 = new GPIO.GPIOPort(26);
+gpioPort26.open(GPIO.PORT_DIRECTION.OUTPUT, function(){console.log("Porta 26 pronta.")});
+
 var gpioPort27 = new GPIO.GPIOPort(27);
 gpioPort27.open(GPIO.PORT_DIRECTION.OUTPUT, function(){console.log("Porta 27 pronta.")});
 
+
+
+
 // Portas de Entrada
+
+var gpioPort12 = new GPIO.GPIOPort(12);
+gpioPort12.open(GPIO.PORT_DIRECTION.INPUT, function(){console.log("Porta 12 pronta.")});
+
+var gpioPort16 = new GPIO.GPIOPort(16);
+gpioPort16.open(GPIO.PORT_DIRECTION.INPUT, function(){console.log("Porta 16 pronta.")});
+
 var gpioPort18 = new GPIO.GPIOPort(18);
 gpioPort18.open(GPIO.PORT_DIRECTION.INPUT, function(){console.log("Porta 18 pronta.")});
+
+var gpioPort20 = new GPIO.GPIOPort(20);
+gpioPort20.open(GPIO.PORT_DIRECTION.INPUT, function(){console.log("Porta 20 pronta.")});
+
+var gpioPort21 = new GPIO.GPIOPort(21);
+gpioPort21.open(GPIO.PORT_DIRECTION.INPUT, function(){console.log("Porta 21 pronta.")});
 
 var gpioPort23 = new GPIO.GPIOPort(23);
 gpioPort23.open(GPIO.PORT_DIRECTION.INPUT, function(){console.log("Porta 23 pronta.")});
@@ -53,11 +88,22 @@ gpioPort23.open(GPIO.PORT_DIRECTION.INPUT, function(){console.log("Porta 23 pron
 var gpioPort24 = new GPIO.GPIOPort(24);
 gpioPort24.open(GPIO.PORT_DIRECTION.INPUT, function(){console.log("Porta 24 pronta.")});
 
+var gpioPort25 = new GPIO.GPIOPort(25);
+gpioPort25.open(GPIO.PORT_DIRECTION.INPUT, function(){console.log("Porta 25 pronta.")});
+
+
 // Array de sensores
+
 var sensores = [];
+sensores[12] = gpioPort12;
+sensores[16] = gpioPort16;
 sensores[18] = gpioPort18;
+sensores[20] = gpioPort20;
+sensores[21] = gpioPort21;
 sensores[23] = gpioPort23;
 sensores[24] = gpioPort24;
+sensores[25] = gpioPort25;
+
 
 // Array com últimas leituras
 var ultimasLeituras = [];
@@ -110,12 +156,37 @@ function onCommand(cmd) {
 	var boolValue = (!cmd.value)? false: true;
 
 	switch(cmd.id){
+	
+	case 04: 
+		gpioPort04.setValue(boolValue);
+		break;
+
+	case 05: 
+		gpioPort05.setValue(boolValue);
+		break;
+
+	case 06: 
+		gpioPort06.setValue(boolValue);
+		break;
+
+	case 13: 
+		gpioPort13.setValue(boolValue);
+		break;
+
 	case 17:
 		gpioPort17.setValue(boolValue);
 		break;
 
+	case 19: 
+		gpioPort19.setValue(boolValue);
+		break;
+
 	case 22:
 		gpioPort22.setValue(boolValue);
+		break;
+
+	case 26:
+		gpioPort26.setValue(boolValue);
 		break;
 
 	case 27:
@@ -131,9 +202,15 @@ function onCommand(cmd) {
 
 // Funções de leitura dos sensores
 function lerSensores(){
+	
+	lerSensor(12);
+	lerSensor(16);
 	lerSensor(18);
+	lerSensor(20);
+	lerSensor(21);
 	lerSensor(23);
 	lerSensor(24);
+	lerSensor(25);
 }
 
 function lerSensor(idSensor){
@@ -147,4 +224,5 @@ function lerSensor(idSensor){
 		ultimasLeituras[idSensor] = leitura;
 	}//if
 }
+
 
